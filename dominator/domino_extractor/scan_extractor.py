10883e2da12d03cbec47824cca4e612ff55ef66d
Joseph Redfern <joseph@redfern.me>
2016-03-15 13:18:43 +0000

    estimate width/height using ratio

--- dominator/domino_extractor/scan_extractor.py
+++ dominator/domino_extractor/scan_extractor.py
@@ -1,7 +1,9 @@
 from dominator.segmentation.threshold import ThresholdSegmentation
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.signal import argrelextrema
 import cv2
+import math
 
 
 class ScanExtractor(object):
@@ -9,13 +11,15 @@ class ScanExtractor(object):
     def __init__(self, scene_path):
         self.image = cv2.imread(scene_path)
 
+        cv2.imshow('Input Image', cv2.resize(self.image.astype(np.uint8), (450, 600)))
+
     def process(self):
-        thresholder = ThresholdSegmentation(~self.image, 200, thresh_type='GRAYSCALE')
+        thresholder = ThresholdSegmentation(~self.image, 220, thresh_type='GRAYSCALE')
         image, mask = thresholder.segment_image()
 
         mask *= 255
 
-        kernel = np.ones((50,50),np.uint8)
+        kernel = np.ones((80,80),np.uint8)
         morphed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=kernel)
 
         labelCount, labelledMask = cv2.connectedComponents(morphed)
@@ -28,28 +32,44 @@ class ScanExtractor(object):
 
         labelledMask = labelledMask.astype(np.uint8)
 
-        cv2.imshow('max', cv2.resize(labelledMask.astype(np.uint8), (300, 400)))
+        cv2.imshow('max', cv2.resize(labelledMask.astype(np.uint8), (450, 600)))
 
         rows = []
         widths = []
 
         for row in labelledMask:
             res = np.where(row==255)
-            leftPos = res[0][0] if len(res[0]) > 1 else res[0] if res[0] != [] else -1
-            rightPos = res[0][-1] if len(res[0]) > 1 else res[0] if res[0] != [] else -1
+            leftPos = res[0][0] if len(res[0]) > 1 else res[0] if len(res[0]) != 0 else -1
+            width = np.where(row[leftPos:] == 0)
 
+            width = width[0][0] if len(width[0]) > 1 else width[0] if len(width[0]) != 0 else -1
+            rightPos = leftPos + width
             widths.append(rightPos-leftPos)
-
             rows.append((leftPos, rightPos))
 
-        hist, _ = np.histogram(widths)
+        hist, bins = np.histogram(widths)
+        peaks = argrelextrema(hist, np.greater)
+
+        min_peak = peaks[0][0] if len(peaks[0]) > 1 else peaks[0]
+        width = int(round(bins[min_peak]))
+
+        height = width*2
+
+        print "First peak is at: {0}, with value {1}".format(min_peak, bins[min_peak])
+        print "Estimated Domino Dimensions: {0}x{1}px".format(width, height)
+
+        annotated = self.image.copy()
+
+        cv2.rectangle(annotated, (0, 0), (width, height), (255, 0, 0), 5)
+        cv2.imshow('annotated', cv2.resize(annotated, (450, 600)))
 
         plt.plot(hist)
 
-        # plt.plot(rows)
+        #plt.plot(rows)
         plt.show()
 
         cv2.waitKey(0)
 
 if __name__ == '__main__':
-    ScanExtractor('../tests/data/image/IMG_4584.JPG').process()
+     ScanExtractor('/Users/joe/Downloads/IMG_4588.JPG').process()
+    #ScanExtractor('/Users/joe/Downloads/potato.png').process()
